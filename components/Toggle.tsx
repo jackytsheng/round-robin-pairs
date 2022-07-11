d81891abfc005920007d7fc822677bd542f0294e
jackytsheng <jackytsheng@gmail.com>
2022-07-12 02:24:42 +1000

    remove svgs and turn into jsx

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
-import Svg, { Icon } from '../svgs/Svg';
+import Svg, { Icon } from './Svg';
 
 type ToggleProps = {
   onToggle: (bool: boolean) => void;
@@ -8,10 +7,7 @@ type ToggleProps = {
 };
 
 const Toggle = ({ onToggle, isChecked }: ToggleProps) => (
-  <label
-    htmlFor='toggle'
-    className='inline-flex relative items-center cursor-pointer'
-  >
+  <label htmlFor='toggle' className='inline-flex items-center cursor-pointer'>
     <input
       type='checkbox'
       value=''
@@ -20,9 +16,9 @@ const Toggle = ({ onToggle, isChecked }: ToggleProps) => (
       id='toggle'
       className='sr-only peer'
     />
-    <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-100 dark:peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-400"></div>
-    <span className='ml-3 text-sm font-medium text-slate-900 dark:text-slate-200'>
-      <Svg icon={Icon.Sun}></Svg>
+    <div className="w-9 h-5 relative bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-100 dark:peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-400"></div>
+    <span className='ml-3 text-sm w-6 font-medium'>
+      <Svg icon={isChecked ? Icon.Moon : Icon.Sun} />
     </span>
   </label>
 );
