3a4f7faab8c51665bf97bd86272913bf063a85b7
jackytsheng <jackytsheng@gmail.com>
2022-07-14 01:13:13 +1000

    change footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -155,14 +155,9 @@ const Home: NextPage = () => {
         </section>
       </main>
       <footer className='flex h-24 w-full items-center justify-center '>
-        <a
-          className='flex items-center justify-center gap-2'
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+        <span className='text-sm text-slate-900 dark:text-slate-200'>
           Created by Jackytsheng
-        </a>
+        </span>
       </footer>
     </div>
   );
